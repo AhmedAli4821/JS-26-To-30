13b423c7d5bfc338981e103e90e9faf9f3e2c53f
Ahmed Ali <121734255+AhmedAli4821@users.noreply.github.com>
2023-03-21 14:21:17 +0500

    JS 26 To 30

---
+++ Java 26 to 30/app.js
@@ -0,0 +1,114 @@
+// document.write("<h1>Chapter. No # 26 To 30</h1>")
+
+// document.write("<h3>__QUESTION NO 1__</h3>"+"</br>")
+// var num = prompt("Enter Number","3.45214")
+// document.write("Number:" + num +"</br>")
+
+// var num1 = Math.round(num)
+// document.write("Round Of Value:" + num1 +"</br>")
+
+// var num2 = Math.floor(num)
+// document.write("Floor Value:" + num2 +"</br>")
+
+// var num3 = Math.ceil(num)
+// document.write("Ceil Value" + num3 +"</br>")
+
+// document.write("<h3>__QUESTION NO 2__</h3>"+"</br>")
+
+// var num = prompt("Enter Number","-2.673")
+// document.write("Number:" + num +"</br>")
+
+// var num1 = Math.round(num)
+// document.write("Round Of Value"  + num1 +"</br>")
+
+// var num2 = Math.floor(num)
+// document.write("Floor Valu" + num2 +"</br>")
+
+// var num3 = Math.ceil(num)
+// document.write("Ceil Value:" + num3 +"</br>")
+
+
+// document.write("<h3>__QUESTION NO 3__</h3>"+"</br>")
+
+// var absolute = prompt("Enter Value","-3")
+// var number = Math.abs(absolute)
+// document.write("The Absolute Value Of:" +absolute+ "is"+number)
+
+
+// document.write("<h3>__QUESTION NO 4__</h3>"+"</br>")
+
+
+// var random = Math.random(random)
+// random = random * 6
+// var random1 = Math.ceil(random)
+// document.write("Randum Ceil value:"+random1)
+
+// document.write("<h3>__QUESTION NO 5__</h3>"+"</br>")
+
+// var coin = Math.random(coin)
+// coin = coin * 2
+// var coinvalue = Math.ceil(coin)
+// document.write(coinvalue)
+// if (coinvalue == 1){
+//     document.write("</br>Random Coin Value:heads")
+// }
+// else{
+//     document.write("</br>Randum Coin Value tails")
+// }
+
+// document.write("<h3>__QUESTION NO 6__</h3>"+"</br>")
+
+// var randomNumber = Math.random(randomNumber)
+// var value = randomNumber * 100;
+// value = Math.ceil(value)
+// document.write("Random Number between 1 and 100:"+value)
+
+
+
+// document.write("<h3>__QUESTION NO 7__</h3>"+"</br>")
+
+// var userInput = prompt("Enter you weight in Kilograms", "58.3kgs")
+// var value = userInput.slice("0","4")
+
+// document.write("The weight of the user is "+ value + "Kilograms")
+
+
+
+// document.weite("<h3>__QUESSTION NO 8__</h3>"+"</br>")
+
+// var randomMatch = Math.random(randomMatch)
+// randomMatch = randomMatch * 10 
+// var randomCeil = Math.ceil(randomMatch)
+// var match = prompt("Enter Nomber from 1 to 10","5")
+// if (match == randomCeil){
+//     document.write("Congratulations")
+// }else{
+//     document.write("Try Again")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
